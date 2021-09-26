6136aff9399e71825625224020e7a11028efc1c5
CharieBlastX7 <61608668+CharieBlastX7@users.noreply.github.com>
2021-09-26 15:39:52 +0530

    Update topwave footer

--- components/TopWaveFooter.js
+++ components/TopWaveFooter.js
@@ -4,11 +4,10 @@ export default function TopWaveFooter() {
   return (
     <div className="outer_box_for_waves">
       <section id="wavesbelownavbar">
-        <svg
+        {/* // eslint-disable-next-line @next/next/no-img-element */}
+        <img
           src="https://res.cloudinary.com/dnv3ztqf1/image/upload/v1605866350/Images%20for%20UNSCRIPT%20ROOKIE/logos/upperwave_dizfuw.svg"
           alt="bottom waves"
-          width="100"
-          height="100"
         />
       </section>
     </div>
