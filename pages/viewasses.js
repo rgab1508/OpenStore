bde5f1595f57077f373fd154a7e1ea7406adbe15
AKSHATA-22 <akshatasangwai1234@gmail.com>
2021-09-26 13:20:55 +0530

    changes

--- pages/viewasses.js
+++ pages/viewasses.js
@@ -34,21 +34,7 @@ export default function Home() {
       <footer>
         <WaveFooter />
         <Footer />
-        <Script
-          src="https://code.jquery.com/jquery-3.3.1.js"
-          integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-          crossOrigin="anonymous"
-        ></Script>
-        <Script
-          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
-          integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
-          crossOrigin="anonymous"
-        ></Script>
-        <Script
-          src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
-          integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
-          crossOrigin="anonymous"
-        ></Script>
+        
       </footer>
     </div>
   );
