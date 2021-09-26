c8bdced1971877e5ee756cd1814c1a63085638bf
theabbie <abhishek7gg7@gmail.com>
2021-09-26 13:33:43 +0530

    build fix

--- pages/_app.js
+++ pages/_app.js
@@ -13,16 +13,19 @@ function MyApp({ Component, pageProps }) {
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          crossOrigin="anonymous"
         />
         {/* // eslint-disable-next-line @next/next/no-page-custom-font */}
         <link
           href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;300&display=swap"
           rel="stylesheet"
+          crossOrigin="anonymous"
         />
         <link
           rel="stylesheet"
           href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
           integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+          crossOrigin="anonymous"
         />
         <script
           src="https://code.jquery.com/jquery-3.3.1.js"
