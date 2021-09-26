0ef33e6cd244b8ba50473697c4fda9c936ae31ea
theabbie <abhishek7gg7@gmail.com>
2021-09-26 13:13:23 +0530

    build fix

--- pages/quickview.js
+++ pages/quickview.js
@@ -4,18 +4,18 @@ import Image from "next/image";
 import Script from "next/script";
 
 import Navbar from "../components/Navbar";
-import Display from "../components/display.js";
+import Display from "../components/Display";
 
-import TopWaveFooter from "../components/topwavefooter.js";
+import TopWaveFooter from "../components/TopWaveFooter";
 
-import Card from "../components/card.js";
-import Team from "../components/team.js";
-import Itemview from "../components/itemview.js";
+import Card from "../components/Card";
+import Team from "../components/Team";
+import Itemview from "../components/ItemView";
 
 
 
-import WaveFooter from "../components/wavefooter.js";
-import Footer from "../components/footer.js";
+import WaveFooter from "../components/WaveFooter";
+import Footer from "../components/Footer";
 
 export default function QuickView() {
     return (
