76294f22e3a45ab0577a187ed437b6a0e59b79f0
CharieBlastX7 <61608668+CharieBlastX7@users.noreply.github.com>
2021-09-26 12:56:45 +0530

    Update create.js page

--- pages/createasset.js
+++ pages/createasset.js
@@ -12,7 +12,6 @@ import Card from "../components/card.js";
 import Team from "../components/team.js";
 import ItemView from "../components/itemview.js";
 
-
 import WaveFooter from "../components/wavefooter.js";
 import Footer from "../components/footer.js";
 
@@ -43,60 +42,84 @@ export default function Createasset() {
         />
       </Head>
 
-      <Navbar />
-
       <main>
+        <h1 className="text-center my-5 header display-4">Create Asset</h1>
 
-        <h1 class="text-center my-5 header display-4">Create Asset</h1>
-
-
-
-        <div class="container ">
-          <div class="row">
-            <div class="col-sm block-to-disappear-in-mobile">
-              <img src="https://res.cloudinary.com/dnv3ztqf1/image/upload/v1632635884/devathon/create-asset_nvz7xi.svg" width="100%" alt="image for add doctor"  />
+        <div style={{ marginBottom: "50px" }} className="container ">
+          <div className="row ">
+            <div className="col-sm-6 block-to-disappear-in-mobile">
+              <Image
+                src="https://res.cloudinary.com/dnv3ztqf1/image/upload/v1632635884/devathon/create-asset_nvz7xi.svg"
+                layout="fill"
+                alt="image htmlFor add doctor"
+              />
             </div>
-            <div class="col-sm">
-              <form action="/adddoctor" method="POST" class="form-group">
-                <input type="hidden" name="csrfmiddlewaretoken" value="fIwiR9rbZTmvxfmW8gC8CiS93Zx36iAh0kdWjuhKGglTMld96xGITqBEbdBR4EkY" />
-                <ul class="unordered-list">
-                  <li><label for="id_name">Asset Name:</label> <input type="text" name="name" maxlength="500" required id="id_name"/></li>
-                    <li><label for="id_description">Asset Description</label> <input type="text" name="phone" maxlength="500" required id="id_description"/></li>
-                      <li><label for="id_price_in_eth">Asset price in ETH:</label> <input type="number" name="id_price_in_eth" value="0" required id="id_price_in_eth"/></li>
-                      <input
-                          type="file"
-                          name="Asset"
-                          className="my-4"
-                      />
+            <div className="col-sm-6">
+              <for action="/adddoctor" method="POST" className="form-group">
+                <input
+                  type="hidden"
+                  name="csrfmiddlewaretoken"
+                  value="fIwiR9rbZTmvxfmW8gC8CiS93Zx36iAh0kdWjuhKGglTMld96xGITqBEbdBR4EkY"
+                />
+                <ul className="unordered-list">
+                  <li>
+                    <label htmlFor="id_name">Asset Name:</label>{" "}
+                    <input
+                      type="text"
+                      name="name"
+                      maxLength="500"
+                      required
+                      id="id_name"
+                    />
+                  </li>
+                  <li>
+                    <label htmlFor="id_description">Asset Description</label>{" "}
+                    <input
+                      type="text"
+                      name="phone"
+                      maxLength="500"
+                      required
+                      id="id_description"
+                    />
+                  </li>
+                  <li>
+                    <label htmlFor="id_price_in_eth">Asset price in ETH:</label>{" "}
+                    <input
+                      type="number"
+                      name="id_price_in_eth"
+                      value="0"
+                      required
+                      id="id_price_in_eth"
+                    />
+                  </li>
+                  <input type="file" name="Asset" classNameName="my-4" />
                 </ul>
-               
-                        <input class="btn btn-block commonbuttons" type="submit"/>
-            </form>
+
+                <input className="btn btn-block commonbuttons" type="submit" />
+              </for>
+            </div>
+          </div>
         </div>
+      </main>
+      <footer>
+        <WaveFooter />
+        <Footer />
+        <Script
+          src="https://code.jquery.com/jquery-3.3.1.js"
+          integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+          crossOrigin="anonymous"
+        ></Script>
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+          integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+          crossOrigin="anonymous"
+        ></Script>
+        <Script
+          src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+          integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+          crossOrigin="anonymous"
+        ></Script>
+      </footer>
     </div>
-</div>
-
-
-            </main>
-                <footer>
-                  <WaveFooter />
-                  <Footer />
-                  <script
-                    src="https://code.jquery.com/jquery-3.3.1.js"
-                    integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-                    crossorigin="anonymous"
-                  ></script>
-                  <script
-                    src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
-                    integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
-                    crossorigin="anonymous"
-                  ></script>
-                  <script
-                    src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
-                    integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
-                    crossorigin="anonymous"
-                  ></script>
-                </footer>
-        </div>
-              );
-          }
+  );
+}
