dcf8c7fe610ba1cef8acd3147c3030e03241d507
Aditya Unnikrishnan <adiunni@hotmail.com>
2021-10-01 22:47:50 +0530

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-export const nftaddress = "0x45aCCb0BA2adb3551DBE9E3B6284667D9116FBad";
-export const nftmarketaddress = "0xA846e8B96b235d586C49DE4E16E9faE9892f2316";
-export const projAddress =
-  "https://polygon-mumbai.infura.io/v3/cfc0ba400dcf4a2ca14edff9f68620fd";
